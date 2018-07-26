648e4d148a3ba806d2bb084db198b9e37aa9269e
Thomas <teleathers22@gmail.com>
2018-07-27 02:41:52 +0800

    add filetype help file to textdocs, add assembler library framework (uses *.tas0 extension)

--- VMSYSTEM/g2asmlib.py
+++ VMSYSTEM/g2asmlib.py
@@ -37,10 +37,11 @@ print("plugins loaded.")
 
 
 
-def xasparse(scrpath, syntaxonly=0):
+def xasparse(scrpath, syntaxonly=0, printprefix=""):
+	ppx=printprefix
 	xasfile=open(scrpath, 'r')
-	print("SBTCVM Assembly Script (XAS) v1")
-	print("running: '" + scrpath + "'\n")
+	print(ppx + "SBTCVM Assembly Script (XAS) v1")
+	print(ppx + "running: '" + scrpath + "'\n")
 	lineno=1
 	for line in xasfile:
 		lineno+=1
@@ -57,45 +58,45 @@ def xasparse(scrpath, syntaxonly=0):
 			if arg!=None:
 				pathx=iofuncts.findtrom(arg, ext=".xas", exitonfail=1, exitmsg="XAS ERROR: xas script: '" + arg + "' was not found. Line: '" + str(lineno) + "' in: '" + scrpath + "'")
 				if pathx!=scrpath:
-					print("----XAS subscript: '" + arg + "'")
-					xasparse(pathx, syntaxonly)
-					print("-Subscript Done.\n")
+					print(ppx + "----XAS subscript: '" + arg + "'")
+					xasparse(pathx, syntaxonly, printprefix+"  ")
+					print(ppx + "-Subscript Done.\n")
 				else:
-					sys.exit("XAS ERROR: Subscript loop error. Line: '" + str(lineno) + "' in: '" + scrpath + "'")	
+					sys.exit(ppx + "XAS ERROR: Subscript loop error. Line: '" + str(lineno) + "' in: '" + scrpath + "'")	
 			else:
-				sys.exit("XAS ERROR: no argument after command: xas. Line: '" + str(lineno) + "' in: '" + scrpath + "'")	
+				sys.exit(ppx + "XAS ERROR: no argument after command: xas. Line: '" + str(lineno) + "' in: '" + scrpath + "'")	
 		elif cmd=="asm":
 			if arg!=None:
 				pathx=iofuncts.findtrom(arg, ext=".tasm", exitonfail=1, exitmsg="XAS ERROR: source file: '" + arg + "' was not found. Line: '" + str(lineno) + "' in: '" + scrpath + "'")
-				print("assemble: '" + arg + "'")
-				assemble(pathx, syntaxonly)
-				print("Done.\n")
+				print(ppx + "assemble: '" + arg + "'")
+				assemble(pathx, syntaxonly, printprefix+"  ")
+				print(ppx + "Done.\n")
 			else:
-				sys.exit("XAS ERROR: no argument after command: asm. Line: '" + str(lineno) + "' in: '" + scrpath + "'")
+				sys.exit(ppx + "XAS ERROR: no argument after command: asm. Line: '" + str(lineno) + "' in: '" + scrpath + "'")
 		elif cmd=="print":
 			if arg!=None:
-				print("--SCRIPT: " + arg)
+				print(ppx + "--SCRIPT: " + arg)
 		elif cmd=="exit":
 			break
 		for cmdobj in xascmds:
 			if cmd==cmdobj.xcmd:
 				if cmdobj.ispython:
 					if cmdobj.takesfile and arg!=None:
-						print("plugin cmd: '" + cmd + "' exec: '" + cmdobj.execstr + "' file argument: '" + arg + "'")
+						print(ppx + "plugin cmd: '" + cmd + "' exec: '" + cmdobj.execstr + "' file argument: '" + arg + "'")
 						if call(['python']+cmdobj.execstr.split(" ")+[arg])!=0:
-							sys.exit("XAS ERROR: plugin command error! cmd:'" + cmd + "' Line: '" + str(lineno) + "' in: '" + scrpath + "'")
-						print("Done.\n")
+							sys.exit(ppx + "XAS ERROR: plugin command error! cmd:'" + cmd + "' Line: '" + str(lineno) + "' in: '" + scrpath + "'")
+						print(ppx + "Done.\n")
 					else:
-						print("plugin cmd: '" + cmd + "' exec: '" + cmdobj.execstr + "'")
+						print(ppx + "plugin cmd: '" + cmd + "' exec: '" + cmdobj.execstr + "'")
 						if call(['python']+cmdobj.execstr.split(" "))!=0:
-							sys.exit("XAS ERROR: plugin command error! cmd:'" + cmd + "' Line: '" + str(lineno) + "' in: '" + scrpath + "'")
-						print("Done.\n")
-	print("xas finished. exiting...")
+							sys.exit(ppx + "XAS ERROR: plugin command error! cmd:'" + cmd + "' Line: '" + str(lineno) + "' in: '" + scrpath + "'")
+						print(ppx + "Done.\n")
+	print(ppx + "xas finished. exiting...")
 	xasfile.close()
 
 
 
-def assemble(pathx, syntaxonly=0):
+def assemble(pathx, syntaxonly=0, pfx=""):
 	#locate source file and extract destination basename and directory for mainloop class.
 	
 	basepath=pathx.rsplit(".", 1)[0]
@@ -104,20 +105,20 @@ def assemble(pathx, syntaxonly=0):
 	
 	#open source file and init mainloop class
 	sourcefile=open(pathx, 'r')
-	mainl=mainloop(sourcefile, bpdir, bpname)
+	mainl=mainloop(sourcefile, bpdir, bpname, pfx)
 	
 	#parse header
 	mainl.headload()
 	
 	#parse each pass in order.
 	if mainl.p0():
-		sys.exit("Syntax Error (pass 0)")
+		sys.exit(pfx + "Syntax Error (pass 0)")
 	mainl.p1()
 	if mainl.p2():
-		sys.exit("Syntax Error (pass 2)")
+		sys.exit(pfx + "Syntax Error (pass 2)")
 	mainl.p3()
 	if mainl.p4():
-		sys.exit("Error: Invalid romdata! (pass 4)")
+		sys.exit(pfx + "Error: Invalid romdata! (pass 4)")
 	if not syntaxonly:
 		mainl.p5()
 	sourcefile.close()
@@ -131,6 +132,7 @@ class instruct:
 		self.keywords=keywords
 		self.prefixes=[]
 		self.opcode=int(opcode)
+		self.nsp=1
 	def p0(self, data, keyword, lineno):
 		if data==None:
 			return 0, None
@@ -151,7 +153,7 @@ class instruct:
 					return 1, keyword+": Line: " + str(lineno) + ": invalid char in ternary data string!"
 		return 0, None
 	#return length in words of memory. needed here for goto refrence label parsing!
-	def p1(self, data, keyword, lineno):
+	def p1(self, data, keyword, lineno, addr):
 		return 1, {}
 	#second syntax check pass:
 	def p2(self, data, keyword, gotos, lineno):
@@ -179,6 +181,7 @@ class rawinst:
 	def __init__(self):
 		self.keywords=["raw"]
 		self.prefixes=[]
+		self.nsp=1
 	def p0(self, datafull, keyword, lineno):
 		if datafull==None:
 			return 1, keyword+": Line: " + str(lineno) + ": 'raw' requires TWO numeric arguments! raw;arga,argb"
@@ -203,7 +206,7 @@ class rawinst:
 						return 1, keyword+": Line: " + str(lineno) + ": invalid char in ternary data string! (" + data + ")"
 		return 0, None
 	#return length in words of memory. needed here for goto refrence label parsing!
-	def p1(self, data, keyword, lineno):
+	def p1(self, data, keyword, lineno, addr):
 		return 1, {}
 	#second syntax check pass:
 	def p2(self, datafull, keyword, gotos, lineno):
@@ -237,10 +240,55 @@ class rawinst:
 		
 		return [[data1res, data2res, lineno]]
 
+class includetas0:
+	def __init__(self):
+		self.keywords=["include"]
+		self.prefixes=[]
+		self.nsp=0
+		self.tas0list={}
+	def p0(self, datafull, keyword, lineno):
+		if datafull==None:
+			return 1, keyword+": Line: " + str(lineno) + ": 'raw' requires tas0 filename as argument!"
+		#load tas0 file.
+		tas0fobj=iofuncts.loadtrom(datafull, ext=".tas0", exitonfail=0)
+		if tas0fobj==None:
+			return 1, keyword+": Line: " + str(lineno) + ": Unable to load tas0 file!"
+		#init mainloop
+		tas0main=mainloop(tas0fobj, ".", "foobar", pfx="--tas0 parse '" + datafull + "':")
+		#enable internal-only mode: tas0
+		tas0main.mode="tas0"
+		#run headloader
+		tas0main.headload()
+		#pass 0
+		if tas0main.p0():
+			return 1, keyword+": Line: " + str(lineno) + ": tas0 pass 0: Syntax Error!"
+		#store tas0 in dict by name.
+		self.tas0list[datafull]=tas0main
+		return 0, None
+	#return length in words of memory. needed here for goto refrence label parsing!
+	def p1(self, data, keyword, lineno, addr):
+		tas0main=self.tas0list[data]
+		#set address start to current main addr for proper handling of tas0 adresses.
+		tas0main.addrstart=addr
+		tas0main.p1()
+		return 1, tas0main.nspdict
+	#second syntax check pass:
+	def p2(self, datafull, keyword, gotos, lineno):
+		tas0main=self.tas0list[datafull]
+		if tas0main.p2():
+			return 1, keyword+": Line: " + str(lineno) + ": tas0 pass 2: Syntax Error!"
+		return 0, None
+	#should return two signed ints or btint objects.
+	def p3(self, data, keyword, gotos, lineno):
+		tas0main=self.tas0list[data]
+		tas0main.p3()
+		return tas0main.datainstlist
+
 class nspacevar:
 	def __init__(self):
 		self.keywords=[]
 		self.prefixes=['v>']
+		self.nsp=1
 	def p0(self, data, keyword, lineno):
 		if not len(keyword)>2:
 			return 1, keyword+": Line: " + str(lineno) + ": No variable name after prefix! (v>varname)"
@@ -263,7 +311,7 @@ class nspacevar:
 					return 1, keyword+": Line: " + str(lineno) + ": invalid char in ternary data string!"
 		return 0, None
 	#return length in words of memory, and if any, custom namespace entries. needed here for goto refrence label parsing!
-	def p1(self, data, keyword, lineno):
+	def p1(self, data, keyword, lineno, addr):
 		if data==None:
 			return 0, {keyword[2:]: 0}
 		elif data.startswith("10x"):
@@ -291,34 +339,7 @@ class nspacevar:
 
 
 #master keyword parser object list:
-instlist=[instruct(["null"], 0),
-rawinst(),
-instruct(["setreg1"], -9841),
-instruct(["setreg2"], -9840),
-instruct(["copy2to1"], -9839),
-instruct(["copy1to2"], -9838),
-instruct(["regswap"], -9837),
-instruct(["invert1"], -9836),
-instruct(["invert2"], -9835),
-instruct(["stop"], -9000),
-instruct(["add"], -9800),#add instructions
-instruct(["add2"], -9799),
-instruct(["adddata1"], -9798),
-instruct(["adddata2"], -9797),
-instruct(["sub"], -9796),#sub instructions
-instruct(["sub2"], -9795),
-instruct(["subdata1"], -9794),
-instruct(["subdata2"], -9793),
-instruct(["mul"], -9792),#mul instructions
-instruct(["mul2"], -9791),
-instruct(["muldata1"], -9790),
-instruct(["muldata2"], -9789),
-instruct(["div"], -9788),#div instructions
-instruct(["div2"], -9787),
-instruct(["divdata1"], -9786),
-instruct(["divdata2"], -9785),
 
-nspacevar()]
 
 nameallowed="abcdefghijklmnopqrstuvwxyz_-1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
@@ -326,17 +347,47 @@ defaultnspace={}
 
 #mainloop class
 class mainloop:
-	def __init__(self, fileobj, basepath, destname, addrstart=libbaltcalc.mni(9)):
+	def __init__(self, fileobj, basepath, destname, addrstart=libbaltcalc.mni(9), pfx=""):
 		self.fileobj=fileobj
 		self.addrstart=addrstart
 		self.destext=".trom"
 		self.basepath=basepath
 		self.destname=destname
-	
+		self.pfx=pfx
+		self.mode="trom"
+		#instruction list
+		self.instlist=[instruct(["null"], 0),
+		rawinst(),
+		instruct(["setreg1"], -9841),
+		instruct(["setreg2"], -9840),
+		instruct(["copy2to1"], -9839),
+		instruct(["copy1to2"], -9838),
+		instruct(["regswap"], -9837),
+		instruct(["invert1"], -9836),
+		instruct(["invert2"], -9835),
+		instruct(["stop"], -9000),
+		instruct(["add"], -9800),#add instructions
+		instruct(["add2"], -9799),
+		instruct(["adddata1"], -9798),
+		instruct(["adddata2"], -9797),
+		instruct(["sub"], -9796),#sub instructions
+		instruct(["sub2"], -9795),
+		instruct(["subdata1"], -9794),
+		instruct(["subdata2"], -9793),
+		instruct(["mul"], -9792),#mul instructions
+		instruct(["mul2"], -9791),
+		instruct(["muldata1"], -9790),
+		instruct(["muldata2"], -9789),
+		instruct(["div"], -9788),#div instructions
+		instruct(["div2"], -9787),
+		instruct(["divdata1"], -9786),
+		instruct(["divdata2"], -9785),
+		includetas0(),
+		nspacevar()]
+		
 	def headload(self):
 		#header load.
 		self.fileobj.seek(0)
-		mode="trom"
 		self.nsp=0
 		self.gotos=dict(defaultnspace)
 		self.doout=1
@@ -349,8 +400,8 @@ class mainloop:
 				line=line.rsplit("#", 1)[0]
 			
 			if line.startswith("head-mode="):
-				mode=line.split("=")[1]
-			
+				if self.mode!="tas0":
+					self.mode=line.split("=")[1]
 			if line.startswith("head-nspout="):
 				if '1' in line.split("=")[1]:
 					self.nsp=1
@@ -362,37 +413,41 @@ class mainloop:
 					linex=linex.replace("\n", "")
 					name, value = linex.split(";")
 					self.gotos[name]=int(value)
-				print("-Include namespace file: '" + nspname + "'")
+				print(self.pfx + "-Include namespace file: '" + nspname + "'")
 				
 			if line.startswith("head-rname="):
 				self.romdestname=line.split("=")[1]
 				if len(self.romdestname)==0:
-					sys.exit("ERROR: header: head-rname: name must be at least 1 character long.")
+					sys.exit(self.pfx + "ERROR: header: head-rname: name must be at least 1 character long.")
 				for char in self.romdestname:
 					if char not in nameallowed:
-						sys.exit("ERROR: header: head-rname: invalid char '" + char + "' in name: '" + self.romdestname + "'")
-				print("Header: rom name override: '" + self.romdestname + "'")
+						sys.exit(self.pfx + "ERROR: header: head-rname: invalid char '" + char + "' in name: '" + self.romdestname + "'")
+				print(self.pfx + "Header: rom name override: '" + self.romdestname + "'")
 			if line.startswith("head-nspname="):
 				self.nspdestname=line.split("=")[1]
 				if len(self.nspdestname)==0:
-					sys.exit("ERROR: header: head-nspname: name must be at least 1 character long.")
+					sys.exit(self.pfx + "ERROR: header: head-nspname: name must be at least 1 character long.")
 				for char in self.nspdestname:
 					if char not in nameallowed:
-						sys.exit("ERROR: header: head-nspname: invalid char '" + char + "' in name: '" + self.nspdestname + "'")
-				print("Header: nsp name override: '" + self.nspdestname + "'")
+						sys.exit(self.pfx + "ERROR: header: head-nspname: invalid char '" + char + "' in name: '" + self.nspdestname + "'")
+				print(self.pfx + "Header: nsp name override: '" + self.nspdestname + "'")
 		
 		#header framework for future alternate assemble modes. i.e. different starting offsets, etc.
-		if mode=="trom":
+		if self.mode=="trom":
 			self.addrstart=libbaltcalc.mni(9)
 			self.destext=".trom"
-			print("Header: head-mode: trom (default)")
-		elif mode=="vars":
+			print(self.pfx + "Header: head-mode: trom (default)")
+		elif self.mode=="tas0":
+			self.destext=".trom"
+			self.nsp=1
+			print(self.pfx + "tas0 parse: head-mode: tas0 (nsp forced on)")
+		elif self.mode=="vars":
 			self.addrstart=libbaltcalc.mni(9)
 			self.destext=".trom"
 			self.doout=0
-			print("Header: head-mode: vars (nspace vars only)")
+			print(self.pfx + "Header: head-mode: vars (nspace vars only)")
 		else:
-			sys.exit("ERROR: header: head-mode: Invalid mode: '" + mode + "'")
+			sys.exit(self.pfx + "ERROR: header: head-mode: Invalid mode: '" + mode + "'")
 		#set assembler rom output filename.
 		self.romoutput=os.path.join(self.basepath, self.romdestname+self.destext)
 		if self.nsp==1:
@@ -403,7 +458,7 @@ class mainloop:
 			self.nspdict=None
 	def p0(self):
 		self.fileobj.seek(0)
-		print("pass 0: syntax check")
+		print(self.pfx + "pass 0: syntax check")
 		lineno=0
 		for line in self.fileobj:
 			lineno+=1
@@ -424,15 +479,15 @@ class mainloop:
 					data=None
 				if data=="":
 					data=None
-				for inst in instlist:
+				for inst in self.instlist:
 					#normal keywords
 					if keyword in inst.keywords:
 						retlist=inst.p0(data, keyword, lineno)
 						if retlist[0]==1:
 							if retlist[1]!=None:
-								print("Syntax Error: "+retlist[1])
+								print(self.pfx + "Syntax Error: "+retlist[1])
 							else:
-								print("Syntax Error!")
+								print(self.pfx + "Syntax Error!")
 							return 1
 					else:
 						#prefix keywords
@@ -441,15 +496,15 @@ class mainloop:
 								retlist=inst.p0(data, keyword, lineno)
 								if retlist[0]==1:
 									if retlist[1]!=None:
-										print("Syntax Error: "+retlist[1])
+										print(self.pfx + "Syntax Error: "+retlist[1])
 									else:
-										print("Syntax Error!")
+										print(self.pfx + "Syntax Error!")
 									return 1
 		return 0
 	def p1(self):
 		self.fileobj.seek(0)
 		
-		print("pass 1: goto reference label prescan")
+		print(self.pfx + "pass 1: goto reference label prescan")
 		lineno=0
 		for line in self.fileobj:
 			lineno+=1
@@ -474,17 +529,17 @@ class mainloop:
 				if data=="":
 					data=None
 				addr=self.addrstart
-				for inst in instlist:
+				for inst in self.instlist:
 					#normal keywords
 					if keyword in inst.keywords:
-						length, nspaceadd=inst.p1(data, keyword, lineno)
+						length, nspaceadd=inst.p1(data, keyword, lineno, addr)
 						if glabel!="":
 							self.gotos[glabel]=addr
 							if self.nspfile!=None:
 								self.nspdict[glabel]=addr
 						if nspaceadd!={}:
 							self.gotos.update(nspaceadd)
-							if self.nspfile!=None:
+							if self.nspfile!=None and inst.nsp:
 								for f in nspaceadd:
 									self.nspdict.update(nspaceadd)
 						addr+=length
@@ -492,18 +547,18 @@ class mainloop:
 						#prefix keywords
 						for pattern in inst.prefixes:
 							if keyword.startswith(pattern):
-								length, nspaceadd=inst.p1(data, keyword, lineno)
+								length, nspaceadd=inst.p1(data, keyword, lineno, addr)
 								if glabel!="":
 									self.gotos[glabel]=addr
 								if nspaceadd!={}:
 									self.gotos.update(nspaceadd)
-									if self.nspfile!=None:
+									if self.nspfile!=None and inst.nsp:
 										for f in nspaceadd:
 											self.nspdict.update(nspaceadd)
 								addr+=length
 	def p2(self):
 		self.fileobj.seek(0)
-		print("pass 2: post-prescan syntax check")
+		print(self.pfx + "pass 2: post-prescan syntax check")
 		lineno=0
 		for line in self.fileobj:
 			lineno+=1
@@ -524,15 +579,15 @@ class mainloop:
 					data=None
 				if data=="":
 					data=None
-				for inst in instlist:
+				for inst in self.instlist:
 					#normal keywords
 					if keyword in inst.keywords:
 						retlist=inst.p2(data, keyword, self.gotos, lineno)
 						if retlist[0]==1:
 							if retlist[1]!=None:
-								print("Syntax Error: "+retlist[1])
+								print(self.pfx + "Syntax Error: "+retlist[1])
 							else:
-								print("Syntax Error!")
+								print(self.pfx + "Syntax Error!")
 							return 1
 					else:
 						#prefix keywords
@@ -541,15 +596,15 @@ class mainloop:
 								retlist=inst.p2(data, keyword, self.gotos, lineno)
 								if retlist[0]==1:
 									if retlist[1]!=None:
-										print("Syntax Error: "+retlist[1])
+										print(self.pfx + "Syntax Error: "+retlist[1])
 									else:
-										print("Syntax Error!")
+										print(self.pfx + "Syntax Error!")
 									return 1
 		return 0
 	def p3(self):
 		self.datainstlist=[]
 		self.fileobj.seek(0)
-		print("pass 3: main parse pass")
+		print(self.pfx + "pass 3: main parse pass")
 		lineno=0
 		for line in self.fileobj:
 			lineno+=1
@@ -570,7 +625,7 @@ class mainloop:
 					data=None
 				if data=="":
 					data=None
-				for inst in instlist:
+				for inst in self.instlist:
 					#normal keywords
 					if keyword in inst.keywords:
 						datinstpairs=inst.p3(data, keyword, self.gotos, lineno)
@@ -582,33 +637,33 @@ class mainloop:
 								datinstpairs=inst.p3(data, keyword, self.gotos, lineno)
 								self.datainstlist.extend(datinstpairs)
 	def p4(self):
-		print("pass 4: Rom validity check")
+		print(self.pfx + "pass 4: Rom validity check")
 		for item in self.datainstlist:
 			#print(item)
 			inst=int(item[0])
 			data=int(item[1])
 			if inst<libbaltcalc.mni(9) or inst>libbaltcalc.mpi(9):
-				print("Out of range instruction word found!")
-				print("source line: " + str(item[2]))
+				print(self.pfx + "Out of range instruction word found!")
+				print(self.pfx + "source line: " + str(item[2]))
 				return 1
 			if data<libbaltcalc.mni(9) or data>libbaltcalc.mpi(9):
-				print("Out of range data word found!")
-				print("source line: " + str(item[2]))
+				print(self.pfx + "Out of range data word found!")
+				print(self.pfx + "source line: " + str(item[2]))
 				return 1
 		if len(self.datainstlist)>19683:
-			print("Memory Space Overflow!")
+			print(self.pfx + "Memory Space Overflow!")
 			return 1
 		return 0
 	def p5(self):
 		if self.nspfile!=None:
-			print("pass 5: generate nsp file.")
+			print(self.pfx + "pass 5: generate nsp file.")
 			nspout=open(self.nspfile, 'w')
 			for f in self.nspdict:
 				if f != None:
 					nspout.write(str(f) + ";" + str(int(self.nspdict[f])) + "\n")
 			nspout.close()
 		if self.doout==1:
-			print("pass 5: build rom.")
+			print(self.pfx + "pass 5: build rom.")
 			outfile=open(self.romoutput, 'w')
 			for item in self.datainstlist:
 				inst=item[0]
