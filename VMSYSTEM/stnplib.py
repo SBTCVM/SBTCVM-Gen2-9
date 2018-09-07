8c1be3c9cc93d6bc64a63b90d989e58e48a74b0f
Thomas <teleathers22@gmail.com>
2018-09-06 20:40:34 -0400

    bump SSTNPL version number.

--- VMSYSTEM/stnplib.py
+++ VMSYSTEM/stnplib.py
@@ -14,8 +14,8 @@ nptype_label=4
 
 #SSTNPL compiler main routine library.
 
-stnpvers='v0.1.0'
-versint=(0, 1, 0)
+stnpvers='v0.2.0'
+versint=(0, 2, 0)
 
 class npvar:
 	def __init__(self, vname, vdata, vtype=nptype_int):
